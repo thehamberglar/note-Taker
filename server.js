b9f004c024fe2f646b9c8e83e53e6efa9e07d2a5
Ethan Hearnsberger <411ethan114@gmail.com>
2022-10-27 21:58:11 -0500

    db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require("./develop/db");
+const { notes } = require("./db");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
